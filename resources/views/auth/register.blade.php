642fa01313510ebec6cf8eebd93fb03fd9e5bdea
Anne Jan Brouwer <brouwer@annejan.com>
2015-04-23 23:47:01 +0200

    adding auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -54,6 +54,7 @@
 								<button type="submit" class="btn btn-primary">
 									Register
 								</button>
+								<a href="/login/facebook" class="btn btn-primary">Log in with Facebook</a>
 							</div>
 						</div>
 					</form>
