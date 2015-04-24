3bb97ffbd4472e304f24d6dc08e65eadb6038ed0
Anne Jan Brouwer <brouwer@annejan.com>
2015-04-24 02:52:34 +0200

    route

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -8,7 +8,7 @@
     @else
         <ul>
             @foreach( $locations as $location )
-                <li><a href="{{ route('locations.show', $location->slug) }}">{{ $location->name }}</a></li>
+                <li><a href="/locations/{{$location->id }}">{{ $location->name }}</a></li>
             @endforeach
         </ul>
     @endif
