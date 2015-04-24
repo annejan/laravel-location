b5e4c1dd6d05e3b85444b99a818ffd245e542e76
Anne Jan Brouwer <brouwer@annejan.com>
2015-04-24 05:22:51 +0200

    nickname is not allways used

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -1,12 +1,17 @@
 <?php namespace App\Repositories;
 
+use App\User;
+
 class UserRepository {
 
     public function findByNameOrCreate($userData) 
     {
+
         return User::firstOrCreate([
-            'name' => $userData->nickname,
-            'email' => $userData->email
+           // 'name' => $userData->nickname,
+            'name' => $userData->name,
+            'email' => $userData->email,
+	    'password' => substr(md5(rand()), 0, 7) // bit lame
         ]);
     
     }
