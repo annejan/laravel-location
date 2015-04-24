3b9ecd46fbd2eb5565d884d38a6659d88d5b235e
Anne Jan Brouwer <brouwer@annejan.com>
2015-04-24 03:50:24 +0200

    controllers and protection off

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -4,6 +4,8 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
+use Input;
+use Redirect;
 
 class LocationController extends Controller {
 
@@ -25,7 +27,7 @@ class LocationController extends Controller {
 	 */
 	public function create()
 	{
-		//
+		return view('locations.create');
 	}
 
 	/**
@@ -35,7 +37,9 @@ class LocationController extends Controller {
 	 */
 	public function store()
 	{
-		//
+		$input = Input::all();
+                \App\Location::create( $input );
+                return Redirect::route('locations.index')->with('message', 'Locations added');
 	}
 
 	/**
@@ -63,23 +67,26 @@ class LocationController extends Controller {
 	/**
 	 * Update the specified resource in storage.
 	 *
-	 * @param  Locations  $location
+	 * @param \App\Location $location
 	 * @return Response
 	 */
-	public function update(Locations  $location)
+	public function update(\App\Location  $location)
 	{
-		//
+		$input = array_except(Input::all(), '_method');
+                $location->update($input);
+                return Redirect::route('locations.show', $location->id)->with('message', 'Location updated.');
 	}
 
 	/**
 	 * Remove the specified resource from storage.
 	 *
-	 * @param  Locations  $location
+	 * @param \App\Locations  $location
 	 * @return Response
 	 */
-	public function destroy(Locations  $location)
+	public function destroy(\App\Location  $location)
 	{
-		//
+		$location->delete();
+                return Redirect::route('locations.index')->with('message', 'Location deleted.');
 	}
 
 }
