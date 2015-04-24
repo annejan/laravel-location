05182580013c5cb0e7aab9daeb5b593fbb0a2d8c
Anne Jan Brouwer <brouwer@annejan.com>
2015-04-24 05:36:23 +0200

    ugly redirect workaround

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller {
 	public function userAuthenticated($user) {
 
 	    header("Location: /locations");
-
+	    exit();
 	    // the official one does not work for some reason.	
             return redirect('/locations');
 	
